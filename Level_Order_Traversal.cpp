42350a2e4648b8cedbd6eff9da24d05ba3161f16
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-03-23 22:41:53 +0800

    Update Level_Order_Traversal.cpp

--- Level_Order_Traversal.cpp
+++ Level_Order_Traversal.cpp
@@ -1,1 +1,28 @@
+#include <queue>
+#include <cstdio>
+
+struct Node{
+  int data;
+  Node *right,*left;
+};
+
+void Levelorder(Node* node){
+
+        std::queue<Node*> q;
+        q.push(node);
+                                                
+        while (!q.empty()){
+
+            Node *current = q.front();
+            q.pop();
+            std::cout << current->data << " ";
+
+            if (current->left != NULL){
+                q.push(current->left);
+            }
+            if (current->right != NULL){   
+                q.push(current->right);
+            }
+        }
+}
 
