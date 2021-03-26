5a9873341df80a82929a982e9e4cf18f14c8cfe1
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-03-26 17:11:03 +0800

    Update BST_Tree.cpp

--- BST_Tree.cpp
+++ BST_Tree.cpp
@@ -1,1 +1,19 @@
+ TreeNode node = root;
 
+ while( !parentStack.isEmpty() || node != null){
+     if (node != null){
+         parentStack.add(node);
+         node = node.left;
+     } else {
+         node = parentStack.pop();
+         if (minValue >= node.val){
+             isBST = false;
+             break;
+         } else {
+             minValue = node.val;
+         }
+         node = node.right;
+     }
+ }
+
+ return isBST;
